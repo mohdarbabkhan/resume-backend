71ea8db4eb138d5307ae342ad09b6f6a8e6af361
Arbab khan <ak2077719@gmail.com>
2022-11-15 14:23:37 +0530

    modified navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -16,7 +16,7 @@ export const Navbar = () => {
           <div class="collapse navbar-collapse" id="navbarNavDropdown">
             <ul class="navbar-nav">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/home">Home</a>
+                <a class="nav-link active" aria-current="page" href="/">Home</a>
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="/about">AboutMe</a>
