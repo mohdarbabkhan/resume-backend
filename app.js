f7491b25d07d65d07ed9e98ad4e75854bcf89a0f
Arbab khan <ak2077719@gmail.com>
2023-01-26 02:32:00 +0530

    modified app.js

--- app.js
+++ app.js
@@ -15,14 +15,7 @@ require('./db/connec');
 app.use(express.json());
 app.use(require('./routes/auth'));
 
-// we want to serve build on Heroku as a frontend
-if(process.env.NODE_ENV=="production"){
-    app.use(express.static("client/build"));
-    const path = require("path");
-    app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
-    })
-}
+
 
 app.listen(port,(req,res)=>{
     console.log(`server started at ${port}`);
--- client/src/index.js
+++
@@ -1,7 +0,0 @@
-import ReactDom from 'react-dom';
-import React from 'react';
-import App from './app';
-
-ReactDom.render(
-<App/>
-,document.getElementById('root'));
